5bb6b0e25d9d464441cae60ba8723c2070afb8bb
m1cha1s <msulej04@gmail.com>
2021-11-19 14:30:10 +0100

    Expanded server and begun work on json parsing

--- lodka_firmware/src/main.cpp
+++ lodka_firmware/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <ArduinoJson.h>
 
 // const char* ssid = "BSR - Uczniowie";
 // const char* pswd = "bednarsk@";
@@ -9,74 +10,53 @@ const char* pswd = "esp-wifi";
 
 WiFiServer server(80);
 
-struct
-{
-  bool foreward, backward, left, right;
-}steering;
+StaticJsonDocument<2048> json_document;
+
+
+void server_loop(void * params);
+
 
 void setup() {
   Serial.begin(9600);
 
-  // WiFi.softAP(ssid, pswd);
-  // IPAddress IP = WiFi.softAPIP();
+  WiFi.softAP(ssid, pswd);
+  IPAddress IP = WiFi.softAPIP();
 
-  WiFi.begin(ssid, pswd);
+  // WiFi.begin(ssid, pswd);
 
-  while(WiFi.status() != WL_CONNECTED)
-  {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
+  // while(WiFi.status() != WL_CONNECTED) {
+  //   delay(1000);
+  //   Serial.println("Connecting to WiFi...");
+  // }
 
   Serial.print("IP Address is: ");
   Serial.println(WiFi.localIP());
 
-  server.begin();
-  
+  server.begin(); 
+
+  xTaskCreate(server_loop, "Server loop", 4096, NULL, 0, NULL);
 }
 
 void loop() {
-  WiFiClient client = server.available();
-  if(client)
-  {
-    while(client.connected())
+  
+}
+
+void server_loop(void * params) {
+  for(;;) {
+    WiFiClient client = server.available();
+    if(client)
     {
-      while(client.available() > 0)
+      client.println("1111");
+      while(client.connected())
       {
-        String cmd = client.readStringUntil('e');
-        // Serial.println(cmd);
-        const char* cCmd = cmd.c_str();
-
-        if(cCmd[0] == 'S') steering.backward = true;
-        if(cCmd[0] == 's') steering.backward = false;
-
-        if(cCmd[0] == 'W') steering.foreward = true;
-        if(cCmd[0] == 'w') steering.foreward = false;
-
-        if(cCmd[0] == 'A') steering.left = true;
-        if(cCmd[0] == 'a') steering.left = false;
-
-        if(cCmd[0] == 'D') steering.right = true;
-        if(cCmd[0] == 'd') steering.right = false;  
+        while(client.available() > 0)
+        {
+          String cmd = client.readString();
+          Serial.println(cmd);
+        }
       }
-      if(steering.backward)
-        Serial.print("F");      
-      else
-        Serial.print("f");
-      if(steering.backward)
-        Serial.print("B");
-      else
-        Serial.print("b");
-      if(steering.left)
-        Serial.print("L");
-      else
-        Serial.print("l");
-      if(steering.right)
-        Serial.print("R");
-      else
-        Serial.print("r");
-      Serial.print("\n");
+      client.stop();
     }
-    client.stop();
+    vTaskDelay(1);
   }
 }
