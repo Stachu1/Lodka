845b546ff4093be31a0f4761398f095df2b74d66
Stachu1 <stach.basiuk@gmail.com>
2021-12-10 13:24:37 +0100

    elo

--- pi_web_control/main.py
+++ pi_web_control/main.py
@@ -23,4 +23,4 @@ def post():
 
 if __name__ == '__main__':
     print("=================\nRunning on public\n=================")
-    app.run(debug=False, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
